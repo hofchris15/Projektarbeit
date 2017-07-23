52cd7c8c036c5084648cb4d3788f38f0f0b7235e
moderste15 <stefan.moder@edu.fh-joanneum.at>
2017-07-23 16:23:00 +0200

    Navbar to file, half finished socket

---
+++ Sources/exe/model/messageModel.swift
@@ -0,0 +1,9 @@
+//
+// SwiftServer (Projektarbeit)
+// Erstellt von Stefan Moder am 23-7-17.
+// FH Joanneum, Kapfenberg
+// Gruppe {[ Hofer Christian, Stefan Moder]}
+// File: messageModel
+//
+
+import Foundation
